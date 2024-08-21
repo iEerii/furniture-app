5a79feffbf7e86efcc0d0d0138a14be637aff113
Ericka Ponce <erickaponce92@gmail.com>
2024-08-20 19:44:03 -0500

    add assets and header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Header } from './components/Header'
+
 import './App.css'
 
 function App() {
 
 	return (
-		<main>
-			
+		<main className='mainContainer'>
+			<Header />
 		</main>
 	)
 }
