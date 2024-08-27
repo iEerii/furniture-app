8aeea23a2a46d7bd8d667df9c1bf34073f1dd7fe
Ericka Ponce <erickaponce92@gmail.com>
2024-08-27 15:23:18 -0500

    add category component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { Header } from './components/Header'
-
 import './App.css'
+import { Header } from './components/Header'
+import { CategorySlide } from './components/CategorySlide'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
 
 	return (
 		<main className='mainContainer'>
 			<Header />
+			<CategorySlide />
 		</main>
 	)
 }
